7b61e261b19c4f603640ffbcaf31ae78c07870f3
Edgar S Fernandes <fernandes.s.edgar@gmail.com>
2017-01-04 21:40:23 -0200

    improved layout distribution space

--- app/src/main/java/br/com/edsilfer/android/demo/ActivityHomePage.kt
+++ app/src/main/java/br/com/edsilfer/android/demo/ActivityHomePage.kt
@@ -7,6 +7,7 @@ import br.com.edsilfer.android.user_onboarding.model.OnBoardingTheme
 import br.com.edsilfer.android.user_onboarding.model.Page
 import br.com.edsilfer.android.user_onboarding.presenter.ActivityUserOnBoarding
 import br.com.edsilfer.kotlin_support.model.xml.Text
+import br.com.tyllt.view.UserOnBoardingConfiguration
 
 /**
  * Created by efernandes on 28/12/16.
@@ -27,7 +28,7 @@ class ActivityHomePage : AppCompatActivity() {
         theme.panelColor = android.R.color.white
 
         val intent = Intent(this, ActivityUserOnBoarding::class.java)
-        intent.putExtra(ActivityUserOnBoarding.ARG_ONBOARDING_THEME, theme)
+        intent.putExtra(ActivityUserOnBoarding.ARG_ONBOARDING_THEME, UserOnBoardingConfiguration.getConfiguration())
         startActivity(intent)
     }
 
---
+++ app/src/main/java/br/com/edsilfer/android/demo/UserOnBoardingConfiguration.kt
@@ -0,0 +1,125 @@
+package br.com.tyllt.view
+
+import br.com.edsilfer.android.demo.R
+import br.com.edsilfer.android.user_onboarding.model.OnBoardingTheme
+import br.com.edsilfer.android.user_onboarding.model.Page
+import br.com.edsilfer.kotlin_support.model.xml.Text
+
+/**
+ * Created by User on 30/12/2016.
+ */
+
+object UserOnBoardingConfiguration {
+
+    fun getConfiguration(): OnBoardingTheme {
+        val pages = arrayListOf<Page>()
+        pages.add(getPage1())
+        pages.add(getPage2())
+        pages.add(getPage3())
+        pages.add(getPage4())
+        pages.add(getPage5())
+
+        val theme = OnBoardingTheme()
+        theme.pages = pages
+        theme.panelColor = android.R.color.white
+
+        return theme
+    }
+
+    private fun getPage5(): Page {
+        val page = Page(
+                R.color.clr_custom_background_page_05,
+                R.string.str_act_user_onboarding_header_page_05,
+                getHeaderStyle(),
+                R.drawable.img_usr_onboard_page_05,
+                R.string.str_act_user_onboarding_subheader1_page_05,
+                getSubHeader1Style(),
+                R.string.str_act_user_onboarding_subheader2_page_05,
+                getSubHeader2Style()
+        )
+        return page
+    }
+
+    private fun getPage4(): Page {
+        val page = Page(
+                R.color.clr_custom_background_page_04,
+                R.string.str_act_user_onboarding_header_page_04,
+                getHeaderStyle(),
+                R.drawable.img_usr_onboard_page_03,
+                R.string.str_act_user_onboarding_subheader1_page_04,
+                getSubHeader1Style(),
+                R.string.str_act_user_onboarding_subheader2_page_04,
+                getSubHeader2Style()
+        )
+        return page
+    }
+
+    private fun getPage3(): Page {
+        val page = Page(
+                R.color.clr_custom_background_page_03,
+                R.string.str_act_user_onboarding_header_page_03,
+                getHeaderStyle(),
+                R.drawable.img_usr_onboard_page_03,
+                R.string.str_act_user_onboarding_subheader1_page_03,
+                getSubHeader1Style(),
+                R.string.str_act_user_onboarding_subheader2_page_03,
+                getSubHeader2Style()
+        )
+        return page
+    }
+
+    private fun getPage2(): Page {
+        val page = Page(
+                R.color.clr_custom_background_page_02,
+                R.string.str_act_user_onboarding_header_page_02,
+                getHeaderStyle(),
+                R.drawable.img_usr_onboard_page_02,
+                R.string.str_act_user_onboarding_subheader1_page_02,
+                getSubHeader1Style(),
+                R.string.str_act_user_onboarding_subheader2_page_02,
+                getSubHeader2Style()
+        )
+        return page
+    }
+
+    private fun getPage1(): Page {
+        val page = Page(
+                R.color.clr_custom_background_page_01,
+                R.string.str_act_user_onboarding_header_page_01,
+                getHeaderStyle(),
+                R.drawable.img_usr_onboard_page_01,
+                R.string.str_act_user_onboarding_subheader1_page_01,
+                getSubHeader1Style(),
+                R.string.str_act_user_onboarding_subheader2_page_01,
+                getSubHeader2Style()
+        )
+        return page
+    }
+
+    private fun getHeaderStyle(): Text {
+        val headerStyle = Text()
+        headerStyle.color = "#FFFFFF"
+        headerStyle.font = "sans-serif-black"
+        headerStyle.size = 32f
+        headerStyle.style = "bold"
+        return headerStyle
+    }
+
+    private fun getSubHeader1Style(): Text {
+        val headerStyle = Text()
+        headerStyle.color = "#FFFFFF"
+        headerStyle.font = "sans-serif"
+        headerStyle.size = 28f
+        headerStyle.style = "bold"
+        return headerStyle
+    }
+
+    private fun getSubHeader2Style(): Text {
+        val headerStyle = Text()
+        headerStyle.color = "#FFFFFF"
+        headerStyle.font = "sans-serif-thin"
+        headerStyle.size = 18f
+        headerStyle.style = "italic"
+        return headerStyle
+    }
+}
