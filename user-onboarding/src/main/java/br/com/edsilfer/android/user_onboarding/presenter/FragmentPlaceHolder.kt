cadac4c03a8b9f0186b945d79c652548d3f2b22a
josefhruska <pepa.hruska@gmail.com>
2017-02-18 23:50:32 +0100

    Custom layout support.
    Option to set custom content layout for each Page object.

--- user-onboarding/src/main/java/br/com/edsilfer/android/user_onboarding/presenter/FragmentPlaceHolder.kt
+++ user-onboarding/src/main/java/br/com/edsilfer/android/user_onboarding/presenter/FragmentPlaceHolder.kt
@@ -30,8 +30,10 @@ class FragmentPlaceHolder : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater!!.inflate(R.layout.fragment_pager_item, container, false)
         val page = arguments.getSerializable(ARG_PAGE) as Page
+
+        val layoutResource = page.contentLayoutResource
+        val rootView = inflater!!.inflate(layoutResource, container, false)
 
         val header = rootView.findViewById(R.id.header) as TextView
         val image = rootView.findViewById(R.id.image) as ImageView
